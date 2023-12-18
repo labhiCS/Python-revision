367812da30ed9b08952d438ffc591b2bf20ccb03
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-18 21:36:47 +0545

    Revising Function

--- revision.py/function_1.py
+++ revision.py/function_1.py
@@ -1,5 +1,14 @@
-#exercise 8.1 FUNCTION
-def display_message():
-    print("I'm revising function lessons.")
+#exercise 8.5 of FUNCTION.
+def describe_city(city_name, country = 'Nepal'):
+    """display information about cities and country"""
+    print(city_name.title() + " is in " + country + ".")
 
-display_message()
+def main():
+    describe_city(city_name = 'gorkha')
+    describe_city(city_name= 'kolkota', country = 'India')
+    describe_city(city_name= 'N.J', country= 'America')
+    describe_city(city_name= 'Tokyo', country = 'Japan')
+    
+
+if __name__ == "__main__":
+    main()
