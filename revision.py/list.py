4b918c7fc543259b75d0ebd054efd8bc6e8c9742
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-20 21:04:45 +0545

    Practicing Lists.

--- revision.py/list.py
+++ revision.py/list.py
@@ -1,6 +1,6 @@
 motorcycles = ["BMW", "Ducati", "Pulsar220","Honda" ]
 message = "My first bike was " + motorcycles[1].title() + "."
 print(message)
+print(motorcycles)
 motorcycles[1] = 'Ns200'
 print(motorcycles)
-print(motorcycles[-1])
