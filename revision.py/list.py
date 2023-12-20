0e244514c27d433d2963bf5b479be5b8ea44ab94
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-20 21:11:35 +0545

    Practicing Lists.

--- revision.py/list.py
+++ revision.py/list.py
@@ -5,6 +5,7 @@ print(motorcycles)
 motorcycles[1] = 'Ns200'
 motorcycles.append("ducati600") #To add
 motorcycles.append("Toyota")
-
+del motorcycles[-1]  #To remove
+del motorcycles[4]
 print(motorcycles)
 print(motorcycles[-4])
