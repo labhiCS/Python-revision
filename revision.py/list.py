d3f4bc9a68973c45ae35078ec0623f01cb484b92
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-20 21:18:35 +0545

    Practicing Lists.

--- revision.py/list.py
+++ revision.py/list.py
@@ -1,11 +1,18 @@
-motorcycles = ["BMW", "Ducati", "Pulsar220","Honda" ]
+motorcycles = ["Pulsar220", "BMW", "Ducati","Honda" ]
+motorcycles.sort()     #To organize
+print(motorcycles)
+print("\n")
+
 message = "My first bike was " + motorcycles[1].title() + "."
 print(message)
-print(motorcycles)
+
 motorcycles[1] = 'Ns200'
 motorcycles.append("ducati600") #To add
 motorcycles.append("Toyota")
-del motorcycles[-1]  #To remove
+
+del motorcycles[-1]     #To remove
 del motorcycles[4]
+motorcycles.remove("BMW")
 print(motorcycles)
-print(motorcycles[-4])
+
+print(motorcycles[-3])
