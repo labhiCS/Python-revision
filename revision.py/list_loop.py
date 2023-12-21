567f5da595c1463c12b2ece4a9d3e872b88ac9d9
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-21 19:07:02 +0545

    Looping through List

--- revision.py/list_loop.py
+++ revision.py/list_loop.py
@@ -1,7 +1,12 @@
 #Practicing loop in list!!
-
-bikes = ["BMW", "ducati", "honda", "splinter"]
+"""bikes = ["BMW", "ducati", "honda", "splinter"]
 for i in bikes:
     print(i.title() + ", it's a great Bike!!")
-    print("I love to ride " + i.title() + ".\n")
+    print("I love to ride " + i.title() + ".\n")"""
 
+# Using range().
+"""for i in range(1, 8):
+    print(i)"""
+
+number = list(range(1, 7))
+print(number)
