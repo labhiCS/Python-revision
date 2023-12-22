078a139e056758126ce0f37d9c7de1bf769e7f47
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-22 23:48:06 +0545

    Practicing UserInput.

--- revision.py/user_input.py
+++ revision.py/user_input.py
@@ -1,8 +1,17 @@
 #Chapter 7 userinput and while loops!!
-user_input = input("Enter any number: ")
+"""user_input = input("Enter any number: ")
 user_input = int(user_input)
 
 if user_input>= 22:
     print("You are old enough to drink alcohol.")
 else:
     print("You are too small for it ")
+"""
+
+#Practicing using while loop!!
+
+current_number = 0
+while current_number <= 20:
+    print(current_number)
+    current_number += 1
+
