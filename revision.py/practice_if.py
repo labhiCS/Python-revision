127ad303d6210269f5e8bd0b690c24978e4b5ad7
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-22 23:25:44 +0545

    Learning if.

--- revision.py/practice_if.py
+++ revision.py/practice_if.py
@@ -1,7 +1,17 @@
-fruits = ["apple", "banana", "mango", "popaya"]
+"""fruits = ["apple", "banana", "mango", "popaya"]
 
 for i in fruits:
     if i == "Apple0":
         print(i.upper() + " = True")
     else:
-        print(i.title())
+        print(i.title()) """
+
+
+age = 22
+
+if age < 12:
+    print("Your cannot dringk alcohol.")
+elif age < 20:
+    print("YOu are  yet not alloweded to dring. ")
+else:
+    print("You can dring alcohol")
