9e8a2e1d4ce0bf13131d4c1c37289fbaad2aa61f
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-24 22:09:21 +0545

    Practicing User_Input

--- revision.py/user_input.py
+++ revision.py/user_input.py
@@ -6,6 +6,7 @@ if user_input>= 22:
     print("You are old enough to drink alcohol.")
 else:
     print("You are too small for it ")
+
 print("\n")
 
 #Practicing using while loop!!
@@ -14,3 +15,5 @@ current_number = 8
 while current_number <= 18:
     print(current_number)
     current_number += 2
+
+#Page no122
