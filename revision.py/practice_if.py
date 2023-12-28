2f8d33d5cb0a2b1046a4ceda573530c2e156d997
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-28 23:59:09 +0545

    Checked

--- revision.py/practice_if.py
+++ revision.py/practice_if.py
@@ -1,7 +1,7 @@
 fruits = ["apple", "banana", "mango", "popaya"]
 
 for i in fruits:
-    if i == "Apple0":
+    if i == "apple":
         print(i.upper() + " = True")
     else:
         print(i.title()) 
