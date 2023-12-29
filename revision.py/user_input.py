a60272169dc5b1ef4cacc864334ab979566ac4c9
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-29 22:49:16 +0545

    Practiced

--- revision.py/user_input.py
+++ revision.py/user_input.py
@@ -1,21 +1,4 @@
 #Chapter 7 userinput and while loops!!
-user_input = input("Enter any number: ")
-user_input = int(user_input)
-
-if user_input>= 20:
-    print("You are old enough to drink alcohol.")
-else:
-    print("You are too small for it ")
-
-print("\n")
-
-#Practicing using while loop!!
-
-current_number = 4
-while current_number <= 18:
-    print(current_number)
-    current_number += 2
-
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
@@ -23,3 +6,10 @@ message = ""
 while message != 'quit':
     message = input(prompt)
     print(message)
+
+#Practicing using while loop!!
+
+current_number = 0
+while current_number <= 15:
+    print(current_number)
+    current_number += 1
