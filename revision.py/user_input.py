ae274cdfb6377cf15568e4b473a1969bac1d0951
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-29 22:48:24 +0545

    Practiced

--- revision.py/user_input.py
+++ revision.py/user_input.py
@@ -16,4 +16,10 @@ while current_number <= 18:
     print(current_number)
     current_number += 2
 
-#Page no122
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
