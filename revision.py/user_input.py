ec7b0d12d1da13462e1ffc08c89f0b1206f1315b
labhiCS <lamichhaneabhi10@gmail.com>
2023-12-29 22:50:55 +0545

    Practiced

--- revision.py/user_input.py
+++ revision.py/user_input.py
@@ -3,9 +3,11 @@ prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
 message = ""
-while message != 'quit':
+while message != 'quit/q':
     message = input(prompt)
-    print(message)
+
+    if message != 'quit/q':
+        print(message)
 
 #Practicing using while loop!!
 
