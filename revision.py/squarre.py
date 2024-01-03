6439c1cfecd14abf189d64ef200c95ee3c6ce8d9
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-03 21:38:51 +0545

    Checked

--- revision.py/squarre.py
+++ revision.py/squarre.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     main() """
 
 squares = []
-for value in range(1,11,2):
+for value in range(1,17):
     square = value*2
     squares.append(square)
     print(squares)
