beae53ae397ca5c23d2a027ee6f2b99b1aa6ba1f
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-05 06:54:04 +0545

    Practicing function

--- function.py/day_1.py
+++ function.py/day_1.py
@@ -2,6 +2,11 @@ def person_name(first_name, last_name):
     print("My name is " + first_name.title() + last_name + ".")
 
 
-person_name("abhishek ", "Lamichhane")
-person_name("anup ", "thapa"  )
-person_name("anusha ", "thapa")
+def main():
+    person_name("abhishek ", "Lamichhane")
+    person_name("anup ", "thapa"  )
+    person_name("anusha ", "thapa")
+    
+
+if __name__ == '__main__':
+    main() 
