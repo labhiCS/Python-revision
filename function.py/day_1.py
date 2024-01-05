3b0e9f304bfa3f9e474082bc18b61841ef5269e5
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-05 06:48:01 +0545

    Practicing function

--- function.py/day_1.py
+++ function.py/day_1.py
@@ -2,4 +2,5 @@ def person_name(first_name, last_name):
     print("My name is " + first_name.title() + last_name + ".")
 
 
-person_name("Abhishek ", "Lamichhane")
+person_name("abhishek ", "Lamichhane")
+person_name("Anup ", "thapa"  )
