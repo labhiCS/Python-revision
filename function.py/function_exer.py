4488ed0d86b3cdb5d95e641f68c86cda07fd1eda
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-07 22:15:19 +0545

    Practicing fun_list

--- function.py/function_exer.py
+++ function.py/function_exer.py
@@ -2,5 +2,17 @@ def show_magicians (names):
     for i in names:
         print(i.title())
 
-magicians = ['abhi', 'anu', 'abhijeet']
+def make_great(magicians):
+    great_magicians = []
+
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ': the Great'
+        great_magicians.append(great_magician)
+
+    for mag in great_magicians:
+        magicians.append(mag)
+
+magicians = ['abhi', 'abhijeet', 'anu', "anup"]
+make_great(magicians)
 show_magicians(magicians)
