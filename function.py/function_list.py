e2efd3948855e0ccf7ce119032c77095dce49580
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-07 22:02:34 +0545

    Practicing fun_list

--- function.py/function_list.py
+++ function.py/function_list.py
@@ -1,6 +1,12 @@
+def main():
+    pass
+    
+if __name__ == "__main__":
+    main()
+
 def user_name(names):
-    for name in names:
-        msg = "Hello, " + name.title() + "!"
+    for i in names:
+        msg = "Hello, " + i.title() + "!"
         print(msg)
         
 person_names = ['anup', 'anusha', 'abhi']
