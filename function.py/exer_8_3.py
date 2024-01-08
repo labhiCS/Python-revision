d5830acc85ad74e915bb4d8f04a78fefee2f5734
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-08 08:20:42 +0545

    Practicing fun_list

---
+++ function.py/exer_8_3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, text_message):
+    print("The size of the t-shirt is " + str(size))
+    print("The messaged printed on a t-shirt is " + text_message.upper())
+
+make_shirt(20, 'ram')
+make_shirt(size = 20, text_message = 'ram')
