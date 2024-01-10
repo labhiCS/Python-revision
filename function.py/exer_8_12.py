ff5a8384a1746b847c2d2a71b84c16514850f5e0
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-10 22:29:37 +0545

    Practicing function

--- function.py/exer_8_12.py
+++ function.py/exer_8_12.py
@@ -1,6 +1,6 @@
 def sandwich_list(*sandwich_names):#pointer
     for i in sandwich_names:
-        print(i.title() + " sandwich is being ordered.")
+        print(i.title() + " sandwich is being ordered." + "\n")
 
 def main():
     sandwich_list('peeporoni')
