703818427dccf772414c70863967c48002b98077
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-12 23:07:15 +0545

    Practicing dictonary

--- dictionary.py/exer_6_1.py
+++ dictionary.py/exer_6_1.py
@@ -1,10 +1,17 @@
+def main():
+    pass
+    
+if __name__ == "__main__":
+    main()  
+
+
 person_name = {
     'first_name': 'Abhishek', 
     'last_name': 'Lamichhane',
     'age': '19',
     'city': 'kathmandu',
     }
-print(person_name["first_name"])
-print(person_name["last_name"])
+
+print(person_name["first_name"] + person_name["last_name"])
 print(person_name["age"] + "yrs old")
 print(person_name["city"] + " city")
