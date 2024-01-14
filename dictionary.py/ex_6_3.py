e90c2260eb294ab2aef28664d3f0a4db5d7ec9a1
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-14 21:39:21 +0545

    Practicing dictonary

--- dictionary.py/ex_6_3.py
+++ dictionary.py/ex_6_3.py
@@ -1,9 +1,9 @@
-prog_words = {
+glossary = {
     'append': 'to add something',
     'pop': 'to remove last element',
     'insert': 'to insert some element anywhere ina list',
     'del': 'to delete',
     'remove': 'to remove elements from list',}
 
-for k, v in prog_words.items():
+for k, v in glossary.items():
     print(k + ": " + v + ".")
