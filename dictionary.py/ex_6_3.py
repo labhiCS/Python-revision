11532538d4badab70683043611402ca8844edda1
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-14 21:41:10 +0545

    Practicing dictonary

--- dictionary.py/ex_6_3.py
+++ dictionary.py/ex_6_3.py
@@ -1,9 +1,9 @@
 glossary = {
     'append': 'to add something',
+    'remove': 'to remove elements from list',
     'pop': 'to remove last element',
     'insert': 'to insert some element anywhere ina list',
-    'del': 'to delete',
-    'remove': 'to remove elements from list',}
+    'del': 'to delete',}
 
 for k, v in glossary.items():
-    print(k + ": " + v + ".")
+    print(k + ": " + v + "." + "\n")
