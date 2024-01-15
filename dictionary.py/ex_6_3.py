95ebd8c73c0db19b107862eef6d48c866841cfe3
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-15 23:32:38 +0545

    Practicing dictonary

--- dictionary.py/ex_6_3.py
+++ dictionary.py/ex_6_3.py
@@ -3,7 +3,9 @@ glossary = {
     'remove': 'to remove elements from list',
     'pop': 'to remove last element',
     'insert': 'to insert some element anywhere ina list',
-    'del': 'to delete',}
+    'del': 'to delete',
+    '.lower()': 'to change element into lowercase',
+    '.sort': 'to sort something',}
 
 for k, v in glossary.items():
     print(k + ": " + v + "." + "\n")
