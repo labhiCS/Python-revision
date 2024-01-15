4306fa072bcd767bc64a5d25584efe0e487fc2f9
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-15 23:42:40 +0545

    Practicing dictonary

--- dictionary.py/dictionary_river.py
+++ dictionary.py/dictionary_river.py
@@ -1,12 +1,12 @@
-rivers = {
+nepal_rivers = {
     'Bishnumati': 'Machhapokhari',
     'Hang Ho': 'China',
     'Nile': 'Egypt',
 }
 
-for k, v in rivers.items():
+for k, v in nepal_rivers.items():
     print(k + " river " + "runs through " + v + ".")
     
 print("\n The following rivers are included in the data: ")
-for key, value in rivers.items():
+for key, value in nepal_rivers.items():
     print(" - " +key + " river.")
