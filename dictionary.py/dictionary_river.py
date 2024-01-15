513c500c46c3e00dbb6de4225c444d0aeea0a2de
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-15 23:39:26 +0545

    Practicing dictonary

---
+++ dictionary.py/dictionary_river.py
@@ -0,0 +1,12 @@
+rivers = {
+    'Bishnumati': 'Machhapokhari',
+    'Hang Ho': 'China',
+    'Nile': 'Egypt',
+}
+
+for k, v in rivers.items():
+    print(k + " river runs through " + v )
+    
+print("The following rivers are included in the data:")
+for key, value in rivers.items():
+    print(key + " river.")
