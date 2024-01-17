e28d7987588a6a7ba4f30f17e8bee5afc314c4d4
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-17 22:46:45 +0545

    Practicing dictonary

--- dictionary.py/dictionary_river.py
+++ dictionary.py/dictionary_river.py
@@ -10,3 +10,9 @@ for k, v in world_rivers.items():
 print("\n The following rivers are included in the data: ")
 for key, value in world_rivers.items():
     print(" - " +key + " river.")
+
+    
+print("\nThe following countries are included in the data: ")
+for key, value in worldrivers.items():
+    print("- " + value)
+    
