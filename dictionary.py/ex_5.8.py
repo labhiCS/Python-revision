81e89da81ff1725dcee0bdddf4c942dd3857f120
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-17 22:50:55 +0545

    Practicing

---
+++ dictionary.py/ex_5.8.py
@@ -0,0 +1,8 @@
+usernames = ["Abhi", "admin", "eric", "abhijeet", "upendra"]
+
+for i in usernames:
+    if i == 'admin':
+        print("hello " + i + " would you like to see a status report?")
+    else:
+        print("hello " + i + " thank you for logging in again")
+
