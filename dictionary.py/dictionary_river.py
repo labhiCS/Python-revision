2a14e23f3e3e68080c5f843aa3e5c935904922c2
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-18 22:59:28 +0545

    no doubelline

--- dictionary.py/dictionary_river.py
+++ dictionary.py/dictionary_river.py
@@ -11,9 +11,7 @@ for k, v in world_rivers.items():
 print("\n The following rivers are included in the data: ")
 for key, value in world_rivers.items():
     print(" - " +key + " river.")
-
-    
+ 
 print("\nThe following countries are included in the data: ")
 for key, value in world_rivers.items():
     print("- " + value)
-    
