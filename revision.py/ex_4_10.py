ec8335e51654718ee53915a55e0e8c6a874dcde6
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-22 09:05:06 +0545

    Practicing list

--- revision.py/ex_4_10.py
+++ revision.py/ex_4_10.py
@@ -1,6 +1,6 @@
 animals = ["dog", "cat", "cow", "buffalo", "bull", "monkey"]
 print("A dog would make a great pet")
-print(animals[0:3])
+print(animals[0:2])
 
 print("three items from the middle of the list are animals")
 print(animals[2:4])
