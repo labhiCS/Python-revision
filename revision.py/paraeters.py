9bc7cd41858e94e9fcabb6e9e31c8d1860526fef
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-25 07:06:39 +0545

    Practicing functions

---
+++ revision.py/paraeters.py
@@ -0,0 +1,12 @@
+def user_name(person_name, age=0):
+    print(person_name.title() + " is " + age + "years old.")
+
+def main():
+    user_name(person_name = 'abhishek', age = '18')
+    user_name(person_name = 'abhi', age= '26')
+
+   
+
+if __name__ == "__main__":
+    main()
+    
