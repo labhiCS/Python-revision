0770142533c1a87e2e538922c20321817956432c
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-26 07:36:38 +0545

    Practicing finction

--- revision.py/paraeters.py
+++ revision.py/paraeters.py
@@ -1,9 +1,9 @@
-def user_name(person_name,last_name,  age=0):
-    print(person_name.title() + " " + last_name + " is " + age + "years old.")
+def user_name(first_name,last_name,  age=0):
+    print(first_name.title() + " " + last_name + " is " + age + "years old.")
 
 def main():
-    user_name(person_name = 'abhi', age= '26', last_name='lamichhane')
-    user_name(person_name="nani", last_name= "simkhada", age = '80')
+    user_name(first_name = 'abhi', age= '26', last_name='lamichhane')
+    user_name(first_name="nani", last_name= "simkhada", age = '80')
 
 if __name__ == "__main__":
     main()
