6b09f81cb384ad75d5d4476a6cc31841797d76be
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-26 07:42:53 +0545

    Practicing function

--- revision.py/paraeters.py
+++ revision.py/paraeters.py
@@ -5,6 +5,7 @@ def main():
     user_name(first_name = 'abhi', age= '26', last_name='lamichhane')
     user_name(first_name="nani", last_name= "simkhada", age = '80')
     user_name(first_name="anup", last_name= "Thapa")
+    user_name(first_name="anusha", last_name="Thapa")
     
 if __name__ == "__main__":
     main()
