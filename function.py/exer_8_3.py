4afae25e3b9702df719152fb104baa5faf46340e
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-28 08:21:43 +0545

    Revised

--- function.py/exer_8_3.py
+++ function.py/exer_8_3.py
@@ -5,8 +5,9 @@ def make_shirt(size, text_message):
 
 def main():
 
-    make_shirt(26, 'PRADA')
+    make_shirt(26, 'nepal')
     make_shirt(size = 20, text_message = 'PRADA')
+    make_shirt(size= 30, text_message='GUCCHI')
 
 if __name__ == "__main__":
     main()
