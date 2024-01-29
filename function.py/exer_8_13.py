43700351664ebb66edf82f52fbb46fbd069dc08b
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-29 08:34:23 +0545

    Practicing function

---
+++ function.py/exer_8_13.py
@@ -0,0 +1,16 @@
+def build_profile(first, last, **user_info):
+    profile = {
+        'first name': first,
+        'last name': last,
+    }
+  
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+def main():
+    user_info = build_profile('abhishek', 'lamichhane')
+    print(user_info)
+
+if __name__ == '__main__':
+    main()
