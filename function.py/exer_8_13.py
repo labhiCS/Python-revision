fea7d073341b81ae3c5698edf870c9bb7cb0aa18
labhiCS <lamichhaneabhi10@gmail.com>
2024-01-31 21:06:26 +0545

    Practicing function

--- function.py/exer_8_13.py
+++ function.py/exer_8_13.py
@@ -10,7 +10,7 @@ def build_profile(first, last, **user_info):
 
 def main():
     user_info = build_profile('abhishek', 'lamichhane', age = 21,address = 'kharibot', profile = 'student')
-    user_info2 = build_profile('anup', 'thapa', age = 26)
+    user_info2 = build_profile('anup', 'thapa', age = 26, profession = 'Army')
     user_info3 = build_profile('anusha', 'thapa', age = 25, address = 'USA', profession='doctor')
      
     print(user_info)
