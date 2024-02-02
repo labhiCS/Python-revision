fdf07643f37eab2a747eee32c5ce30a3f3f21384
labhiCS <lamichhaneabhi10@gmail.com>
2024-02-02 07:57:57 +0545

    Practicing class

--- revision.py/class_exer.py
+++ revision.py/class_exer.py
@@ -6,7 +6,7 @@ class Resturant():
 
     def describe_resturant(self):
         print("My resturant name is " + self.resturant_name.title())
-        print(self.cuisine_type.title() + " is available.")
+        print(self.cuisine_type + " is available.")
     
     def open_resturant(self):
         print(self.resturant_name.title()+ " is opened")
@@ -14,3 +14,4 @@ class Resturant():
 rest_1 = Resturant('Gorkhali Vansaghar', 'Neplese dish')
 
 rest_1.describe_resturant()
+rest_1.open_resturant()
