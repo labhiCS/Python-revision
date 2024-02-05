4912e309600e84d78d4c45840c0f7b0aa9cf5ef1
labhiCS <lamichhaneabhi10@gmail.com>
2024-02-05 09:01:20 +0545

    Practicing class

---
+++ revision.py/exer_8_12.py
@@ -0,0 +1,13 @@
+def sandwich_list(*sandwich_names):
+    for i in sandwich_names:
+        print(i + " sandwich has been ordered.")
+
+def main():
+    sandwich_list('nutella')
+    sandwich_list('veg', 'cheesi')
+    sandwich_list('chicken', 'grilled')
+    
+if __name__ == '__main__':
+    main()
+
+
