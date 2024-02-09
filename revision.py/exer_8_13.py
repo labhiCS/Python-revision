fea2d1b43ac8d196791f65161709c3761cc04eeb
labhiCS <lamichhaneabhi10@gmail.com>
2024-02-09 07:50:45 +0545

    Revising Class

--- revision.py/exer_8_13.py
+++ revision.py/exer_8_13.py
@@ -9,7 +9,7 @@ def build_profile(first, last, **user_info):
     return profile
 
 def main():
-    user_info = build_profile('abhishek', 'lamichhane', age = 19, address = 'kharibot')
+    user_info = build_profile('abhishek', 'lamichhane', age = 19, address = 'kharibot', profile = 'student')
     print(user_info)
 
 if __name__ == '__main__':
