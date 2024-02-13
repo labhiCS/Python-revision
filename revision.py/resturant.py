1d90819f9da64b7cc45cbc3004a4dc48cce95a97
labhiCS <lamichhaneabhi10@gmail.com>
2024-02-13 23:24:34 +0545

    Revising class

--- revision.py/resturant.py
+++ revision.py/resturant.py
@@ -11,7 +11,7 @@ class User():
     def greet_user(self):
         print("Hello, " + self.first_name + " " + self.last_name + ".")
 
-user = User('abhi', 'guragain', 16, 'student.', 'swimmer')
+user = User('abhi', 'guragain', 16, 'student.')
 user_1 = User('anu', 'thapa', 25, 'doctor. ')
 
 user.describe_user()
