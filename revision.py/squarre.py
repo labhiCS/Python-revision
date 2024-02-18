b0a26a4de1f0f2afc59d9fd09d36f96f954b8dc9
labhiCS <lamichhaneabhi10@gmail.com>
2024-02-18 20:49:35 +0545

    Initial commit

--- revision.py/squarre.py
+++ revision.py/squarre.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     main() """
 
 squares = []
-for value in range(1,21):
-    square = value*2
+for value in range(1,11):
+    square = value**2
     squares.append(square)
     print(squares)
