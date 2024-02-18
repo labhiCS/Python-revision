98dca947ecf0898913b370160e129ee6169eeb3c
labhiCS <lamichhaneabhi10@gmail.com>
2024-02-18 20:56:38 +0545

    Initial commit

--- revision.py/paraeters.py
+++ revision.py/paraeters.py
@@ -4,7 +4,7 @@ def user_name(first_name,last_name,  age=24):
 def main():
     user_name(first_name = 'abhi', age= 26, last_name='lamichhane')
     user_name(first_name="nani", last_name= "simkhada", age = 80)
-    user_name(first_name="anup", last_name= "Thapa")
+    user_name(first_name="anup", last_name= "Thapa", age= 29)
     user_name(first_name="anusha", last_name="Thapa")
     
 if __name__ == "__main__":
