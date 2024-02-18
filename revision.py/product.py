47d39cc3db88794738093d7af0a8d55299f17931
labhiCS <lamichhaneabhi10@gmail.com>
2024-02-18 20:53:58 +0545

    Product

---
+++ revision.py/product.py
@@ -0,0 +1,6 @@
+a = int(input("Enter your first Number: "))
+b = int(input("Enter your second Number: "))
+c = int(input("Enter your third Number: "))
+
+product = a * b 
+print("The product is: ", product)
