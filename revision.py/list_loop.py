d385a8fe60566436deb6cc615b3b49171c18cbee
labhiCS <lamichhaneabhi10@gmail.com>
2024-03-14 08:29:30 +0545

    Initial commit

--- revision.py/list_loop.py
+++ revision.py/list_loop.py
@@ -2,7 +2,7 @@
 bikes = ["BMW", "ducati", "honda", "splinter"]
 for i in bikes:
     print(i.title() + ", it's a great Bike!!")
-    print("I love to ride " + i.title() + ".\n")
+    print("I love to ride a " + i.title() + ".\n")
 
 # Using range().
 for i in range(1, 8):
