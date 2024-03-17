b711486a3607b92feb6cfd574016c4a2044d8c11
labhiCS <lamichhaneabhi10@gmail.com>
2024-03-17 22:17:21 +0545

    Initial commmit

--- revision.py/paraeters.py
+++ revision.py/paraeters.py
@@ -1,11 +1,12 @@
-def user_name(first_name,last_name,  age=24):
+def user_name(first_name,last_name, address="", age=24):
     print(first_name.title() + " " + last_name + " is " + str(age) + "years old.")
+    print(first_name.title() + " " + last_name + " " + address)
 
 def main():
     user_name(first_name = 'abhi', age= 26, last_name='lamichhane')
     user_name(first_name="nani", last_name= "simkhada", age = 80)
     user_name(first_name="anup", last_name= "Thapa", age= 29)
-    user_name(first_name="apsara", last_name="Adhakari")
+    user_name(first_name="apsara", last_name="Adhakari", address= "thankot")
     
 if __name__ == "__main__":
     main()
