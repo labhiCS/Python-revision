cdb97c5a5b8fbf8805e46d0c25895c914408e307
labhiCS <lamichhaneabhi10@gmail.com>
2024-03-19 07:45:52 +0545

    functions

--- revision.py/exer_8_13.py
+++ revision.py/exer_8_13.py
@@ -12,6 +12,8 @@ def build_profile(first, last, middle,  **user_info):
 def main():
     user_info = build_profile('abhishek', 'lamichhane', 'bahadur', age = 19, profile = 'student')
     print(user_info)
+    stunent_info = build_profile('abhijeet', 'thapa')
+    print(stunent_info)
     print("\n")
 
 if __name__ == '__main__':
