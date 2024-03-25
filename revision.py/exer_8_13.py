ec8a90681fffcd44b68cd87f71ca2d57635bca01
labhiCS <lamichhaneabhi10@gmail.com>
2024-03-25 22:33:52 +0545

    Initial commit

--- revision.py/exer_8_13.py
+++ revision.py/exer_8_13.py
@@ -12,9 +12,11 @@ def build_profile(first, last, middle,  **user_info):
 def main():
     user_info = build_profile('Abhishek', 'Lamichhane', 'Bahadur', age = 19, profile = 'student')
     print(user_info)
-    stunent_info = build_profile('Abhijeet', 'Thapa', 'Bikram', profile = 'student', Address = 'Kharibot')
+    stunent_info1 = build_profile('Abhijeet', 'Thapa', 'Bikram', profile = 'student', Address = 'Kharibot')
     print(stunent_info)
     print("\n")
+    stunent_info2 = build_profile('Anusha', 'Thapa', Address = 'USA')
+    print(stunent_info2)
 
 if __name__ == '__main__':
     main()
