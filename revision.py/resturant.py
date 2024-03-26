164741cf86dba61b7ac9ad106602e417dfd13d3f
labhiCS <lamichhaneabhi10@gmail.com>
2024-03-26 07:09:26 +0545

    Practicing function

--- revision.py/resturant.py
+++ revision.py/resturant.py
@@ -13,7 +13,7 @@ class User():
 
 user = User('abhi', 'guragain', 16, 'student.')
 user_1 = User('anu', 'thapa', 25, 'doctor. ')
-
+user_2 = User('abijeet', 'lamichhane')
 user.describe_user()
 user.greet_user()
 print("\n")
