579f94a0f9a390830e47e6eb4bd0c201854dfb02
labhiCS <lamichhaneabhi10@gmail.com>
2024-03-27 23:27:42 +0545

    Square number

--- revision.py/squarre.py
+++ revision.py/squarre.py
@@ -1,11 +1,10 @@
-"""def main():
-    pass
-
-if __name__ == '__main__':
-    main() """
-
 squares = []
 for value in range(1,11):
-    square = value**2
+    square = value*2
     squares.append(square)
+
+def main():
     print(squares)
+
+if __name__ == '__main__':
+    main() 
